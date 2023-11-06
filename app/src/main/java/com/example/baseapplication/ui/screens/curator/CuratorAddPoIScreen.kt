49b955adb6ae29f168279f22375178e7a4398fe8
David Raposo <92717795+Raposo06@users.noreply.github.com>
2023-11-06 17:44:54 +0000

    AddPOI finished

--- app/src/main/java/com/example/baseapplication/ui/screens/curator/CuratorAddPoIScreen.kt
+++ app/src/main/java/com/example/baseapplication/ui/screens/curator/CuratorAddPoIScreen.kt
@@ -9,23 +9,45 @@ import android.util.Log
 import android.widget.Toast
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.selection.selectable
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.RadioButton
+import androidx.compose.material3.RadioButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.material3.contentColorFor
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -33,16 +55,25 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.TextUnitType
 import androidx.compose.ui.unit.dp
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.viewmodel.compose.viewModel
 import coil.compose.AsyncImage
+import com.example.baseapplication.R
 import com.example.baseapplication.utlis.PermissionLauncerFactory
 import com.example.baseapplication.models.PointOfInterestTypeEnum
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import com.google.android.gms.location.LocationServices
 import com.google.android.gms.location.Priority.PRIORITY_HIGH_ACCURACY
+import com.google.android.play.integrity.internal.y
 import java.util.Locale
 
 @Composable
@@ -95,6 +126,19 @@ fun CuratorAddPoIContent(
     var isLoading by remember {
         mutableStateOf(false)
     }
+
+    Image(
+        painter = painterResource(id = R.drawable.city_of_aveiro),
+        contentDescription = "Background Image",
+        modifier = Modifier.fillMaxSize(),
+        contentScale = ContentScale.Crop // Crop the image to fill the screen
+    )
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Black.copy(alpha = 0.6f))
+    )
 
     // CAMERA UTILITIES //
     val cameraUtility = CameraUtility(context)
@@ -201,12 +245,28 @@ fun CuratorAddPoIContent(
             TextField(
                 value = uiState.poi.name,
                 onValueChange = onNameChange,
-                label = { Text(text = "Name") })
+                label = { Text(text = "Name") },
+                singleLine = true,
+                shape = MaterialTheme.shapes.small.copy(CornerSize(8.dp)), // Define as bordas arredondadas
+                colors = TextFieldDefaults.textFieldColors(
+                    textColor = MaterialTheme.colorScheme.onSurface,
+                    contentColorFor(backgroundColor = MaterialTheme.colorScheme.surfaceVariant),
+                    cursorColor = MaterialTheme.colorScheme.primary,
+                    focusedIndicatorColor = MaterialTheme.colorScheme.primary
+                ),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(40.dp)
+            )
 
             Column(
                 horizontalAlignment = Alignment.Start,
             ) {
-                Text(text = "Categoria:")
+                Text(
+                    text = "Categoria:",
+                    style = MaterialTheme.typography.titleLarge,
+                    modifier = Modifier.padding(bottom = 16.dp)
+                )
 
                 categoryOptions.forEach { value ->
                     Row(
@@ -217,17 +277,22 @@ fun CuratorAddPoIContent(
                                     onOptionSelected(value)
                                     onTypeChange(value.name)
                                 }
-                            ),
+                            )
+                            .fillMaxWidth()
+                            .padding(vertical = 8.dp),
                         verticalAlignment = Alignment.CenterVertically,
-
-                        ) {
+                    ) {
                         RadioButton(
                             selected = (value == selectedOption),
-                            onClick = { onOptionSelected(value) }
+                            onClick = { onOptionSelected(value) },
+                            colors = RadioButtonDefaults.colors(
+                                selectedColor = MaterialTheme.colorScheme.primary,
+                                unselectedColor = MaterialTheme.colorScheme.onSurfaceVariant
+                            )
                         )
                         Text(
                             text = value.name,
-                            style = MaterialTheme.typography.bodyLarge.merge(),
+                            style = MaterialTheme.typography.bodyLarge,
                             modifier = Modifier.padding(start = 16.dp)
                         )
                     }
@@ -235,47 +300,121 @@ fun CuratorAddPoIContent(
             }
         }
 
+        Spacer(modifier = Modifier.height(200.dp))
 
         if (uiState.localImageUri?.path?.isNotEmpty() == true) {
             Log.d(TAG, "Rendering Image: ${uiState.localImageUri}")
 
-            AsyncImage(model = uiState.localImageUri.toString(), contentDescription = "")
-            Button(onClick = onImgDelete) {
-                Text(text = "Remove Photo")
+            AsyncImage(model = uiState.localImageUri.toString(), contentDescription = "Capture Image", modifier = Modifier
+                .size(400.dp)
+                .width(200.dp)
+                .offset(y = (-130).dp))
+            ElevatedButton(
+                onClick = onImgDelete,
+                shape = RoundedCornerShape(50), // Bordas arredondadas para o botão
+                colors = ButtonDefaults.elevatedButtonColors(
+                    containerColor = MaterialTheme.colorScheme.primary, // Cor de fundo do botão
+                    contentColor = MaterialTheme.colorScheme.onPrimary // Cor do texto e ícone dentro do botão
+                ),
+                modifier = Modifier
+                    //.fillMaxWidth() // Ocupar a largura máxima disponível
+                    .padding(horizontal = 16.dp, vertical = 8.dp) // Espaçamento externo
+                    .width(200.dp)
+                    .height(50.dp)
+            ) {
+                Text(
+                    text = "Remove Photo",
+                    style = MaterialTheme.typography.labelLarge.copy(color = MaterialTheme.colorScheme.onPrimary)
+                )
             }
 
 
         } else {
-            Button(onClick = { onAddPhotoClick() }) {
-                Text(text = "Adicionar Foto")
+            ElevatedButton(
+                onClick = { onAddPhotoClick() },
+                shape = RoundedCornerShape(50), // Bordas arredondadas
+                colors = ButtonDefaults.elevatedButtonColors(
+                    containerColor = MaterialTheme.colorScheme.primary, // Cor de fundo do botão
+                    contentColor = MaterialTheme.colorScheme.onPrimary // Cor do texto e ícone
+                ),
+                modifier = Modifier
+                     // Ocupar a largura máxima disponível
+                    .padding(horizontal = 8.dp, vertical = 8.dp) // Espaçamento externo
+                    .width(200.dp)
+                    .height(50.dp)
+            ) {
+                Icon(
+                    imageVector = Icons.Filled.Add, // Substitua pelo seu recurso de ícone de "check"
+                    contentDescription = "Add", // Descrição para acessibilidade
+                    modifier = Modifier.padding(end = 8.dp) // Espaço entre o ícone e o texto
+                )
+                Text(
+                    text = "Adicionar Foto",
+                    style = MaterialTheme.typography.labelLarge.copy(color = MaterialTheme.colorScheme.onPrimary)
+                )
             }
         }
 
         if (uiState.poi.address.isEmpty()) {
-            Button(onClick = { onAddLocationClick() })
-            {
-                Text(text = "Adicionar Local")
+            ElevatedButton(
+                onClick = { onAddLocationClick() },
+                shape = RoundedCornerShape(50), // Bordas arredondadas para um design suave
+                colors = ButtonDefaults.elevatedButtonColors(
+                    containerColor = MaterialTheme.colorScheme.primary, // Cor de fundo do botão
+                    contentColor = MaterialTheme.colorScheme.onPrimary // Cor do texto e ícone dentro do botão
+                ),
+                modifier = Modifier
+                    //.fillMaxWidth() // Ocupar a largura máxima disponível
+                    .padding(horizontal = 16.dp, vertical = 8.dp) // Espaçamento externo
+                    .width(200.dp)
+                    .height(50.dp)
+            ) {
+                Icon(
+                    imageVector = Icons.Filled.Home, // Substitua pelo seu recurso de ícone de "check"
+                    contentDescription = "Add", // Descrição para acessibilidade
+                    modifier = Modifier.padding(end = 8.dp) // Espaço entre o ícone e o texto
+                )
+                Text(
+                    text = "Adicionar Local",
+                    style = MaterialTheme.typography.labelLarge.copy(color = MaterialTheme.colorScheme.onPrimary)
+                )
             }
+
         } else {
             Text(text = uiState.poi.address)
         }
 
 
         if (!isLoading) {
-            Button(
+            ElevatedButton(
                 onClick = {
                     if (!isLoading) {
                         isLoading = true
                         onCreatePoIClick()
                         isLoading = false
                     }
-
                 },
-
+                // Estilos do botão, com uma cor de elevação para adicionarem profundidade
+                colors = ButtonDefaults.elevatedButtonColors(
+                    containerColor = MaterialTheme.colorScheme.primary,
+                    contentColor = MaterialTheme.colorScheme.onPrimary
+                ),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 8.dp) // Espaço vertical para estética
+            ) {
+                Icon(
+                    imageVector = Icons.Filled.Check, // Substitua pelo seu recurso de ícone de "check"
+                    contentDescription = "Check", // Descrição para acessibilidade
+                    modifier = Modifier.padding(end = 8.dp) // Espaço entre o ícone e o texto
                 )
-            {
-                Text(text = "Criar Local")
+                Text(
+                    text = "Criar Local",
+                    style = MaterialTheme.typography.labelMedium, // Estilo de texto padrão para botões
+                    fontSize = TextUnit(18f, TextUnitType.Sp) // Aumenta a letra para dar mais ênfase
+                )
             }
+
         }
     }
 }
